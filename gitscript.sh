9f2cdf1d6a9b5fd9e6f263a71e96f75988baf119
radhakrishna101087 <radhakrishna101087@gmail.com>
2020-11-07 01:49:38 +0000

    Adding multiple files

---
+++ gitscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/radhakrishna101087/mysecondproject.git
+git pull origin master
+touch ABC DEF GHI XYZ.txt
+git add -A
+git commit -a -m "Adding multiple files"
